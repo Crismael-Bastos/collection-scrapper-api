d29f7c25ba9ca3aaac0463d2102e824fe30c6271
Crismael Bastos <crismaelbastos@gmail.com>
2024-01-07 06:08:48 -0300

    feat: allow all cors

--- api.py
+++ api.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from services import get_magic_eden_collection_top_sales
 
 app = FastAPI()
-
-@app.get('/{time}&{minimum_sales}')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get('/get-top-sales/{time}&{minimum_sales}')
 def top_sales(time: str, minimum_sales: str):
     """ time options: 10m, 1h.
         sales minimum: at least 30 to get good results.
